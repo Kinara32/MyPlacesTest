5141b8e1b5e7d9b6f07846b2cdd93e4d38285b33
Matvei Bykadorov <2kydepsta2@gmail.com>
2022-09-30 16:39:05 +0300

    App Icon Launch Screen Row Hight

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -26,16 +26,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        var contentCell = cell.defaultContentConfiguration()
-        contentCell.text = restraurantNames[indexPath.row]
-        contentCell.image = UIImage(named: restraurantNames[indexPath.row])
-        contentCell.textProperties
-        contentCell.imageProperties
-        cell.contentConfiguration = contentCell
-//        cell.textLabel?.text = restraurantNames[indexPath.row]
-//        cell.imageView?.image = UIImage(named: restraurantNames[indexPath.row])
+//        var contentCell = cell.defaultContentConfiguration()
+//        contentCell.text = restraurantNames[indexPath.row]
+//        contentCell.image = UIImage(named: restraurantNames[indexPath.row])
+//        contentCell.textProperties
+//        contentCell.imageProperties
+//        cell.contentConfiguration = contentCell
+        cell.textLabel?.text = restraurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restraurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         return cell
     }
 
+    // MARK: - Table view delegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
 }
 
