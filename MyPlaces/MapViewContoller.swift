f2b23da03f999f4e3ded48214d364f2e6049ac73
Matvei Bykadorov <2kydepsta2@gmail.com>
2023-01-20 00:23:56 +0300

    add Map View

---
+++ MyPlaces/MapViewContoller.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewContoller.swift
+//  MyPlaces
+//
+//  Created by Matvei Bykadorov on 20.01.2023.
+//
+
+import UIKit
+
+class MapViewContoller: UIViewController {
+
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+}
